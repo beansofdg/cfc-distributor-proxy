f8e285ab828cd353f13fee5e8dc7a5fa7ec728e3
Garvey <kbeans.com@gmail.com>
2026-05-04 02:58:55 -0400

    fix SOAPAction header

--- server.js
+++ server.js
@@ -281,8 +281,8 @@ async function fetchSportsSouthRaw({
       responseType: "arraybuffer",
       headers: {
         "Content-Type": "text/xml; charset=utf-8",
-        SOAPAction:"http://webservices.theshootingwarehouse.com/smart/Inventory.asmx/DailyItemUpdate",
-        Accept: "text/xml",
+        "SOAPAction":"http://webservices.theshootingwarehouse.com/smart/Inventory.asmx/DailyItemUpdate",
+        "Accept": "text/xml",
       },
     }
   );
